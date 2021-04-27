e805063e2655c99ee401a7e224f9eaf1a5f763e5
Code <mac92j@pm.me>
2021-04-26 21:34:28 -0400

    Add form input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -25,7 +26,10 @@ function App() {
 
 
   return (
-    <Expenses expenses={expenses} />
+    <div>
+      <NewExpense />
+      <Expenses expenses={expenses} />
+    </div>
   );
 }
 
